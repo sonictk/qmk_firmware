6f10ac5d0405852aa8e2fc809bb82b1f02776d4f
Siew Yi Liang <admin@sonictk.com>
2022-07-24 01:17:40 -0700

    updated keymap to match current ergodox layout as best as possible

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/sonictk/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/sonictk/keymap.c
@@ -18,20 +18,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x7(
         // left hand
         KC_ESC,    KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_LGUI,
-        KC_GRV,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_ESC,
-        KC_PGUP,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_TAB,
+        KC_GRV,    KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_TAB,
+        KC_PGUP,   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_ESC,
         KC_PGDN,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
         KC_APP,    KC_INS,  KC_LGUI, KC_LCTL,
-                                    KC_LSFT, KC_LCTL,  TT(_FN),
-                                    KC_SPC, TT(_NUMPAD), KC_LALT,
+            TT(_NUMPAD),  // bot left
+            TT(_FN),      // top left
+            KC_LCTL,      // mid bot
+            KC_SPC,       // mid top
+            KC_LSFT,      // bot right 
+            KC_LALT,      // top right
         // right hand
                           KC_CAPS, KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_FIND,
-                          KC_RGUI, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_EQL,
-                          KC_ENT,  KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+                          KC_ENT,  KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_EQL,
+                          KC_RGUI, KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
                                    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_MINS,
                                             KC_RCTL, KC_RGUI,  KC_HOME,  KC_END,
-             KC_RCTL, KC_RSFT, KC_BSPC,
-             TT(_FN), KC_RALT, TT(_NUMPAD)
+            TT(_FN),      // top right
+            TT(_NUMPAD),  // bot right
+            KC_BSPC,      // mid top
+            KC_RCTL,      // mid bot
+            KC_RALT,      // left top
+            KC_RSFT       // left bot
     ),
 
     [_FN] = LAYOUT_5x7(
@@ -41,23 +49,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MPLY,   _______,   KC_LEFT,    KC_DOWN,  KC_RGHT,   _______,  _______,
         KC_MNXT,   _______,   _______,    _______,  _______,   _______,
         KC_MPRV,   KC_MSTP,   _______,    _______,
-                                    _______, _______,
-                                    _______, _______,
-                                    _______, _______,
+                                    _______,
+                                    _______,
+                                    _______,
+                                    _______,
+                                    _______,
+                                    _______,
         // right hand
                           KC_NUM,    KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,
                           KC_HOME,   KC_PGUP,   KC_LCBR,   KC_RCBR,   KC_LBRC,   KC_RBRC,   KC_F12,
                           _______,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   KC_INS,    KC_SYRQ,
                                      KC_PGDN,   KC_END,    KC_PAUS,   KC_SCRL,   KC_BSLS,   KC_PIPE,
                                                            _______,   _______,   _______,   _______,
-             _______, KC_DEL, _______,
-             _______, _______, _______
+                                    _______,
+                                    _______,
+                                    KC_DEL,
+                                    _______,
+                                    _______,
+                                    _______
     ),
 
     [_NUMPAD] = LAYOUT_5x7(
         // left hand
         QK_BOOT,   _______,   _______,   _______,   _______,   _______,  _______,
-        QK_RBT,   _______,   _______,   KC_MS_U,   _______,   _______,  _______,
+        QK_RBT,   _______,   _______,    KC_MS_U,   _______,   _______,  _______,
         _______,   _______,   KC_MS_L,   KC_MS_D,   KC_MS_R,   _______,  _______,
         _______,   _______,   _______,   _______,   _______,   _______,
         _______,   _______,   _______,   _______,
