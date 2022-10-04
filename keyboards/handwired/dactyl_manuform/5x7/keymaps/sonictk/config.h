0f7de82ada56c491064c7981f85e16aab13aa0da
Siew Yi Liang <admin@sonictk.com>
2022-10-04 16:05:38 -0700

    revert to using 3 taps since 2 taps is too easy to activate

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/sonictk/config.h
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/sonictk/config.h
@@ -24,8 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define EE_HANDS
 
-// Allow for tapping toggle to activate with just 2 taps instead of 5.
-#define TAPPING_TOGGLE 2
+// Allow for tapping toggle to activate with just 3 taps instead of 5.
+#define TAPPING_TOGGLE 3
 
 // A custom combo for activating secondary function layer by hitting FN1 + FNNum.
 #define COMBO_COUNT 1
